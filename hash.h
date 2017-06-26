a727349c13593e10e85a8d4fc99edff399ad1e78
MatthewC221 <matthewchen42@gmail.com>
2017-06-26 17:56:24 +1000

    Added mem management, delete keys and hash

--- hash.h
+++ hash.h
@@ -77,32 +77,49 @@ void put (Hash * H, int cur_key, int cur_value)
     
     int_node * tmp = H->K[gen_key]; 
     
+    // Empty LL
     if (tmp == NULL) {
         H->K[gen_key] = new_N;
         new_N->next = NULL;
     } else {
         
+        // Key to override at head
         if (tmp->next == NULL && tmp->k == cur_key) {
             tmp->v = cur_value;
             printf("Overriden hash value %s%d%s at index %s%d%s\n", YELLOW, cur_value, END, YELLOW, gen_key, END);
+            free(new_N);
             return;
         }
-    
+        
+        // Key to override in middle
         while (tmp->next != NULL) {
             if (tmp->k == cur_key) {
                 tmp->v = cur_value;
                 printf("Overriden hash value %s%d%s at index %s%d%s\n", YELLOW, cur_value, END, YELLOW, gen_key, END);
+                free(new_N);
                 return;
             } 
             tmp = tmp->next;
         }
+        
+        // Key to override at very end
+        if (tmp->k == cur_key) {
+            tmp->v = cur_value;
+            printf("Overriden hash value %s%d%s at index %s%d%s\n", YELLOW, cur_value, END, YELLOW, gen_key, END);
+            free(new_N);
+            return;
+        } 
+            
+        // If no conflicts insert at tail
         tmp->next = new_N;
         new_N->next = NULL;
     }
         
+        
     return;
 }
 
+//** Prints all indexes and elements of the hash
 void printHash (Hash * H) 
 {
     printf("    Hash\n");
@@ -121,9 +138,82 @@ void printHash (Hash * H)
     }
 }
 
+void free_hash (Hash * H) 
+{
+    for (int i = 0; i < H->cur_size; i++) {
+        int_node *del = H->K[i];
+        while (del != NULL) {
+            int_node *tmp = del;
+            del = del->next;
+            free(tmp);
+        }
+        free(H->K[i]);
+        H->K[i] = NULL;
+    }
+    free(H->K);
+    free(H);
+}
+
+
+//** Frees a node, if that node doesn't exist print err
+void free_node(Hash * H, int key) 
+{
+    int gen_key = 0;
+    
+    // Don't mod by 0
+    if (key) gen_key = key % H->cur_size;
+    
+    int_node * del_before = H->K[gen_key]; 
+    int_node * del_after = H->K[gen_key];
+    
+    // printf("The generated key = %d\n", gen_key);
+    
+    if (del_after == NULL) {
+        fprintf(stderr, "<Hash.h>: illegal removal of non-existent key\n");
+        return;
+    } else {
+    
+        bool exists = false;
+        while (del_after->next != NULL) {       
+            if (del_after->k == key) {
+                if (del_after == H->K[gen_key]) {            // Deleting the head
+                    if (del_after->next == NULL) {
+                        H->K[gen_key] = NULL;
+                    } else {                    
+                        H->K[gen_key] = del_after->next;
+                    }
+                } else if (del_after->next != NULL) {
+                    int_node * new_next = del_after->next;
+                    del_before->next = new_next;
+                } else if (del_after->next == NULL) {
+                    del_before->next = NULL;
+                }
+                exists = true;
+                free(del_after);
+                break;
+            }
+            del_before = del_after;
+            del_after = del_after->next;
+        }
+        
+        if (!exists && del_after->k == key) {
+            if (del_after == H->K[gen_key]) {
+                H->K[gen_key] = NULL;
+            } else {
+                del_before->next = NULL;
+            }
+            free(del_after);
+        } else if (!exists) {
+            fprintf(stderr, "<Hash.h>: illegal removal of non-existent key\n");
+        }
+    }
+
+}
+
+//** Creates a node
 int_node * createNode(int cur_key, int cur_value)
 {
-    int_node * N = malloc(sizeof(struct int_node));
+    int_node * N = malloc(sizeof(int_node));
     N->k = cur_key;
     N->v = cur_value;
     N->next = NULL;
@@ -131,6 +221,7 @@ int_node * createNode(int cur_key, int cur_value)
     return N;   
 }
 
+//** Finds next prime during resizing
 int nextPrime(int num)
 {
 
@@ -143,6 +234,7 @@ int nextPrime(int num)
     return -1;
 }
 
+//** Determines if prime
 int isPrime(int num)
 {
 
--- test.c
+++ test.c
@@ -17,6 +17,20 @@ int main(int argc, char *argv[])
     
     printHash(H);
     
+    /*
+    for (int i = 0; i < 20; i++) {
+        free_node(H, i);
+        printHash(H);
+    }
+    
+    free_node(H, 421);
+    free_node(H, 512);
+    free_node(H, 1);
+    
+    */
+    printHash(H);
+    //free_hash(H);
+    printHash(H);
 
 
 
