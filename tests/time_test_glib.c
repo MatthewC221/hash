9b392443c46e1757927b8b1779da85a91b9a2ac4
MatthewC221 <matthewchen42@gmail.com>
2017-09-08 01:02:12 +1000

    Freeing is really strange in glib...

--- tests/time_test_glib.c
+++ tests/time_test_glib.c
@@ -2,6 +2,7 @@
 #include <glib.h>
 #include <stdlib.h>
 #include <time.h>
+#include <inttypes.h>
 #include <assert.h>
 
 int main(int argc, char *argv[]) 
@@ -15,18 +16,32 @@ int main(int argc, char *argv[])
 
 	    int *key = NULL;
 	    int *val = NULL;
-	    
-		GHashTable *hash = g_hash_table_new_full (g_int_hash, g_int_equal, free, free);
+	    gint *k_one = NULL;
+
+		GHashTable *hash = g_hash_table_new_full (g_int_hash, g_int_equal, free, NULL);
 		// int * keys = malloc(sizeof(int) * size);
 
 		for (int i = 0; i < size; i++) {
+			/*
 		  	key = malloc( sizeof(*key) );
 		  	*key = i;
 		  	val = malloc( sizeof(*val) );
 		  	*val = 1;
-		  	g_hash_table_insert(hash, key, val);
+			*/
+		  	k_one = g_new(gint, 1);
+		  	*k_one = i;
+		  	g_hash_table_insert(hash, key, GINT_TO_POINTER(1));
 		}
 
+		/*
+		for (int i = 0; i < size; i++) {
+			key = malloc(sizeof(*key));
+			*key = i;
+			gpointer *p = g_hash_table_lookup(hash, key);
+			assert (GPOINTER_TO_INT(p) == i);
+			free(key);
+		}	
+		*/
 		g_hash_table_destroy(hash);
 	} else {
 		fprintf(stderr, "Usage ./test <size>\n");
